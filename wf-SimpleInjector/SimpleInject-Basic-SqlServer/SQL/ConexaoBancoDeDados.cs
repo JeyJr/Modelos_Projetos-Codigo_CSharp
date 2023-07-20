e7c78e541b9a3b8ceae5b7c7cb578418b627d8d6
José Junior <jeyjr.dg@gmail.com>
2023-07-19 22:02:12 -0300

    SimpleInjector-Basic-SqlServer
    Modelo base de utilização SimpleInjector com sqlServer

---
+++ wf-SimpleInjector/SimpleInject-Basic-SqlServer/SQL/ConexaoBancoDeDados.cs
@@ -0,0 +1,22 @@
+﻿using SimpleInject_Basic_SqlServer.SQL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInject_Basic_SqlServer.SQL
+{
+    public class ConexaoBancoDeDados : IConexaoBancoDeDados
+    {
+
+        private static string autenticacaoWindows = "Data Source = INSTANCIA;Initial Catalog = BASE DE DADOS; Integrated Security = True;";
+        public string ObterStringConexao()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(autenticacaoWindows);
+            builder.TrustServerCertificate = true;
+            return builder.ConnectionString;
+        }
+    }
+}
---
+++ wf-SimpleInjector/SimpleInject-Basic-SqlServer/SQL/Interfaces/ICRUDSimples.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInject_Basic_SqlServer.SQL.Interfaces
+{
+    public interface ICRUDSimples
+    {
+        DataTable Select(string sql);
+        int Delete(string sql);
+        int Insert(string sql);
+        int Update(string sql);
+    }
+}
