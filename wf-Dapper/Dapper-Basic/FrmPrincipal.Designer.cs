7255321a66a26c1be6d1d920edcfd6bca6c5aa8e
José Junior <jeyjr.dg@gmail.com>
2023-07-22 15:49:14 -0300

    Dapper select

--- wf-Dapper/Dapper-Basic/FrmPrincipal.Designer.cs
+++ wf-Dapper/Dapper-Basic/FrmPrincipal.Designer.cs
@@ -28,19 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            btnExibir = new Button();
+            dtgProduto = new DataGridView();
+            colPK_ID = new DataGridViewTextBoxColumn();
+            colNome = new DataGridViewTextBoxColumn();
+            colPreco = new DataGridViewTextBoxColumn();
+            colDescricao = new DataGridViewTextBoxColumn();
+            colAtivo = new DataGridViewTextBoxColumn();
+            btnPesquisarID = new Button();
+            txtBoxID = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dtgProduto).BeginInit();
             SuspendLayout();
+            // 
+            // btnExibir
+            // 
+            btnExibir.Location = new Point(539, 117);
+            btnExibir.Name = "btnExibir";
+            btnExibir.Size = new Size(156, 29);
+            btnExibir.TabIndex = 0;
+            btnExibir.Text = "Todos Produtos";
+            btnExibir.UseVisualStyleBackColor = true;
+            btnExibir.Click += btnExibir_Click;
+            // 
+            // dtgProduto
+            // 
+            dtgProduto.AllowUserToAddRows = false;
+            dtgProduto.AllowUserToDeleteRows = false;
+            dtgProduto.AllowUserToResizeColumns = false;
+            dtgProduto.AllowUserToResizeRows = false;
+            dtgProduto.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = SystemColors.Control;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dtgProduto.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dtgProduto.ColumnHeadersHeight = 35;
+            dtgProduto.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dtgProduto.Columns.AddRange(new DataGridViewColumn[] { colPK_ID, colNome, colPreco, colDescricao, colAtivo });
+            dtgProduto.EnableHeadersVisualStyles = false;
+            dtgProduto.Location = new Point(12, 152);
+            dtgProduto.Name = "dtgProduto";
+            dataGridViewCellStyle5.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = SystemColors.Control;
+            dataGridViewCellStyle5.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle5.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = DataGridViewTriState.True;
+            dtgProduto.RowHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            dtgProduto.RowHeadersVisible = false;
+            dtgProduto.RowHeadersWidth = 50;
+            dtgProduto.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dtgProduto.RowTemplate.Height = 29;
+            dtgProduto.Size = new Size(683, 330);
+            dtgProduto.TabIndex = 1;
+            // 
+            // colPK_ID
+            // 
+            colPK_ID.HeaderText = "PK_ID";
+            colPK_ID.MinimumWidth = 6;
+            colPK_ID.Name = "colPK_ID";
+            colPK_ID.Visible = false;
+            colPK_ID.Width = 125;
+            // 
+            // colNome
+            // 
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.Font = new Font("Microsoft Sans Serif", 7.8F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle2.Padding = new Padding(0, 5, 0, 0);
+            colNome.DefaultCellStyle = dataGridViewCellStyle2;
+            colNome.HeaderText = "Produto";
+            colNome.MinimumWidth = 6;
+            colNome.Name = "colNome";
+            colNome.ReadOnly = true;
+            colNome.Width = 200;
+            // 
+            // colPreco
+            // 
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.TopRight;
+            dataGridViewCellStyle3.Font = new Font("Microsoft Sans Serif", 7.8F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle3.Format = "R$ ###.##0";
+            dataGridViewCellStyle3.NullValue = null;
+            dataGridViewCellStyle3.Padding = new Padding(0, 0, 5, 0);
+            colPreco.DefaultCellStyle = dataGridViewCellStyle3;
+            colPreco.HeaderText = "Preço";
+            colPreco.MinimumWidth = 6;
+            colPreco.Name = "colPreco";
+            colPreco.ReadOnly = true;
+            colPreco.Width = 140;
+            // 
+            // colDescricao
+            // 
+            colDescricao.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.Font = new Font("Microsoft Sans Serif", 7.8F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle4.Padding = new Padding(0, 3, 0, 0);
+            colDescricao.DefaultCellStyle = dataGridViewCellStyle4;
+            colDescricao.HeaderText = "Descrição";
+            colDescricao.MinimumWidth = 6;
+            colDescricao.Name = "colDescricao";
+            colDescricao.ReadOnly = true;
+            // 
+            // colAtivo
+            // 
+            colAtivo.HeaderText = "Ativo";
+            colAtivo.MinimumWidth = 6;
+            colAtivo.Name = "colAtivo";
+            colAtivo.ReadOnly = true;
+            colAtivo.Visible = false;
+            colAtivo.Width = 125;
+            // 
+            // btnPesquisarID
+            // 
+            btnPesquisarID.Location = new Point(143, 115);
+            btnPesquisarID.Name = "btnPesquisarID";
+            btnPesquisarID.Size = new Size(156, 29);
+            btnPesquisarID.TabIndex = 2;
+            btnPesquisarID.Text = "Pesquisar ID";
+            btnPesquisarID.UseVisualStyleBackColor = true;
+            btnPesquisarID.Click += btnPesquisarID_Click;
+            // 
+            // txtBoxID
+            // 
+            txtBoxID.Location = new Point(12, 117);
+            txtBoxID.Name = "txtBoxID";
+            txtBoxID.Size = new Size(125, 27);
+            txtBoxID.TabIndex = 3;
+            txtBoxID.KeyPress += txtBoxNumerosInteiros_KeyPress;
+            txtBoxID.Leave += txtBoxLimitarCasasDecimais_Leave;
             // 
             // FrmPrincipal
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(531, 505);
+            ClientSize = new Size(707, 505);
+            Controls.Add(txtBoxID);
+            Controls.Add(btnPesquisarID);
+            Controls.Add(dtgProduto);
+            Controls.Add(btnExibir);
             Name = "FrmPrincipal";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Principal";
+            Load += FrmPrincipal_Load;
+            ((System.ComponentModel.ISupportInitialize)dtgProduto).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnExibir;
+        private DataGridView dtgProduto;
+        private Button btnPesquisarID;
+        private TextBox txtBoxID;
+        private DataGridViewTextBoxColumn colPK_ID;
+        private DataGridViewTextBoxColumn colNome;
+        private DataGridViewTextBoxColumn colPreco;
+        private DataGridViewTextBoxColumn colDescricao;
+        private DataGridViewTextBoxColumn colAtivo;
     }
 }
--- wf-Dapper/Dapper-Basic/FrmPrincipal.cs
+++ wf-Dapper/Dapper-Basic/FrmPrincipal.cs
@@ -1,10 +1,122 @@
+using Dapper_Basic.Base;
+using Dapper_Basic.Interfaces;
+using Dapper_Basic.Repositorio;
+
 namespace Dapper_Basic
 {
     public partial class FrmPrincipal : Form
     {
+        IProdutoRepository produtoRepository;
+
         public FrmPrincipal()
         {
             InitializeComponent();
         }
+
+
+        #region Eventos
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+            string stringConexao = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            produtoRepository = new ProdutoRepository(stringConexao);
+        }
+
+        //Formatação dos TextBox
+        private void txtBoxNumerosInteiros_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtBoxApenasNumerosComCasasDecimais_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '.' || e.KeyChar == ';')
+            {
+                e.KeyChar = ',';
+            }
+
+            if (((TextBox)sender).Text.Contains(',') && e.KeyChar == ',')
+            {
+                e.Handled = true;
+            }
+
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != ',' && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtBoxLimitarCasasDecimais_Leave(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            if (textBox is null)
+                return;
+
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+                return;
+
+            decimal valor = Convert.ToDecimal(textBox.Text);
+            valor = Math.Round(valor, 2);
+            textBox.Text = valor.ToString();
+        }
+
+
+        //Btns
+        private void btnExibir_Click(object sender, EventArgs e)
+        {
+            IEnumerable<Produto> itens = produtoRepository.ObterTodosProdutos();
+            //dtgProduto.DataSource = itens;
+
+            if (dtgProduto.Rows.Count > 0)
+                dtgProduto.Rows.Clear();
+
+            itens.OrderBy(i => i.NOME)
+                .ToList()
+                .ForEach(i => ExibirProdutosNoDataGrid(i, false));
+        }
+
+        private void btnPesquisarID_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtBoxID.Text))
+                return;
+
+            int id = Convert.ToInt16(txtBoxID.Text.Trim());
+
+            Produto item = produtoRepository.ObterProdutoPorId(id);
+
+            if (item is null)
+                return;
+
+            ExibirProdutosNoDataGrid(item);
+
+            //List<Produto> listaProdutos = new List<Produto> { item };
+            //dtgProduto.DataSource = listaProdutos;
+
+            //listaProdutos.OrderBy(p => p.NOME)
+            //    .ToList()
+            //    .ForEach(p => ExibirProdutosNoDataGrid(p));
+        }
+
+        private void ExibirProdutosNoDataGrid(Produto produto, bool limparGrid = true)
+        {
+            if (limparGrid && dtgProduto.Rows.Count > 0)
+                dtgProduto.Rows.Clear();
+
+            dtgProduto.Rows.Add(
+                produto.PK_ID,
+                produto.NOME,
+                produto.VALOR,
+                produto.DESCRICAO,
+                produto.ATIVO
+                );
+        }
+
+        #endregion Eventos
+
+
     }
 }
