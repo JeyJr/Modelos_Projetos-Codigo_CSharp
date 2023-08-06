340610a5d82d1cad6f383cf0230b9120d6a55cc3
José Junior <jeyjr.dg@gmail.com>
2023-08-06 15:53:35 -0300

    wf-TiposGenericos

---
+++ wf-TiposGenericos/TiposGenericosBase/FrmPrincipal.cs
@@ -0,0 +1,28 @@
+namespace TiposGenericosBase
+{
+    public partial class FrmPrincipal : Form
+    {
+        public List<object> Registros = new List<object>();
+        public FrmCadastro FrmCadastro { get; set; }
+
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMarca_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCategoria_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEmbalagem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
