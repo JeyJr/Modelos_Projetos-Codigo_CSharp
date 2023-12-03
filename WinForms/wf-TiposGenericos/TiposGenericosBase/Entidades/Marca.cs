9cac20c54f92f2f5ad7418d0e7dcf7e4984eb45c
José Junior <jeyjr.dg@gmail.com>
2023-12-03 16:01:33 -0300

    WinForms_TiposGenericos

---
+++ WinForms/wf-TiposGenericos/TiposGenericosBase/Entidades/Marca.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposGenericosBase.Entidades
+{
+    public class Marca
+    {
+        public string Nome { get; set; }
+    }
+}
