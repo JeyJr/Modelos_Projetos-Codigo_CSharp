74a73feec6b8f0eb07f6abf512feba047b6be03e
José Junior <jeyjr.dg@gmail.com>
2023-12-03 18:45:49 -0300

    SimpleInjector

---
+++ WinForms/wf-SimpleInjector-Exemplos/SimpleInject-Basic-SqlServer/FrmPrincipal.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace SimpleInject_Basic_SqlServer
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dtgPrincipal = new DataGridView();
+            btnExecutar = new Button();
+            txtConsulta = new TextBox();
+            lblNome = new Label();
+            ((System.ComponentModel.ISupportInitialize)dtgPrincipal).BeginInit();
+            SuspendLayout();
+            // 
+            // dtgPrincipal
+            // 
+            dtgPrincipal.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dtgPrincipal.Location = new Point(12, 100);
+            dtgPrincipal.Name = "dtgPrincipal";
+            dtgPrincipal.RowHeadersWidth = 51;
+            dtgPrincipal.RowTemplate.Height = 29;
+            dtgPrincipal.Size = new Size(760, 390);
+            dtgPrincipal.TabIndex = 0;
+            // 
+            // btnExecutar
+            // 
+            btnExecutar.Location = new Point(617, 59);
+            btnExecutar.Name = "btnExecutar";
+            btnExecutar.Size = new Size(155, 28);
+            btnExecutar.TabIndex = 1;
+            btnExecutar.Text = "Executar";
+            btnExecutar.UseVisualStyleBackColor = true;
+            btnExecutar.Click += btnExecutar_Click;
+            // 
+            // txtConsulta
+            // 
+            txtConsulta.Location = new Point(12, 22);
+            txtConsulta.Name = "txtConsulta";
+            txtConsulta.Size = new Size(760, 27);
+            txtConsulta.TabIndex = 5;
+            // 
+            // lblNome
+            // 
+            lblNome.Location = new Point(12, 52);
+            lblNome.Name = "lblNome";
+            lblNome.Size = new Size(89, 23);
+            lblNome.TabIndex = 6;
+            lblNome.Text = "new Query";
+            // 
+            // FrmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(784, 502);
+            Controls.Add(lblNome);
+            Controls.Add(txtConsulta);
+            Controls.Add(btnExecutar);
+            Controls.Add(dtgPrincipal);
+            Name = "FrmPrincipal";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Principal";
+            Load += FrmPrincipal_Load;
+            ((System.ComponentModel.ISupportInitialize)dtgPrincipal).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dtgPrincipal;
+        private Button btnExecutar;
+        private TextBox txtConsulta;
+        private Label lblNome;
+    }
+}
---
+++ WinForms/wf-SimpleInjector-Exemplos/SimpleInjector-IEnumerator-Basic/ProdutoConsultas.cs
@@ -0,0 +1,55 @@
+﻿using SimpleInjector_IEnumerator_Basic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInjector_IEnumerator_Basic
+{
+    public class ProdutoConsultas : IProdutoConsultas
+    {
+        public List<Produto> produtos;
+
+        public ProdutoConsultas()
+        {
+            produtos = new List<Produto>
+            {
+                new Produto { ID = 1, Nome = "Produto 1", Preco = 10.99m, QuantidadeEstoque = 100 },
+                new Produto { ID = 2, Nome = "Produto 2", Preco = 20.50m, QuantidadeEstoque = 50 },
+                new Produto { ID = 3, Nome = "Produto 3", Preco = 5.75m, QuantidadeEstoque = 200 }
+            };
+        }
+
+        public void AtualizarProduto(Produto produto)
+        {
+            Produto produtoExistente = produtos.Find(p => p.ID == produto.ID);
+            if (produtoExistente != null)
+            {
+                produtoExistente.Nome = produto.Nome;
+                produtoExistente.Preco = produto.Preco;
+                produtoExistente.QuantidadeEstoque = produto.QuantidadeEstoque;
+            }
+        }
+
+        public void InserirProduto(Produto produto)
+        {
+            produtos.Add(produto);
+        }
+
+        public Produto? ObterProdutoPorID(int id)
+        {
+            return produtos.Find(p => p.ID == id);
+        }
+
+        public IEnumerable<Produto> ObterTodosProdutos()
+        {
+            return produtos;
+        }
+
+        public void RemoverProduto(int id)
+        {
+            produtos.RemoveAll(p => p.ID == id);
+        }
+    }
+}
