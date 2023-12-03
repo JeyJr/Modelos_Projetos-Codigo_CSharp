cad54b272f4955c5c669cd477793efd022be21b6
José Junior <jeyjr.dg@gmail.com>
2023-12-03 18:41:37 -0300

    WinForms_Dapper

---
+++ WinForms/wf-Dapper/Dapper-Basic/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,19 @@
+﻿using Dapper_Basic.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper_Basic.Interfaces
+{
+    public interface IProdutoRepository
+    {
+        IEnumerable<Produto> ObterTodosProdutos();
+        IEnumerable<Produto> ObterLista(string personalizado);
+        Produto ObterProdutoPorId(int id);
+        void InserirProduto(Produto produto);
+        void AtualizarProduto(Produto produto);
+        void ExcluirProduto(int id);
+    }
+}
