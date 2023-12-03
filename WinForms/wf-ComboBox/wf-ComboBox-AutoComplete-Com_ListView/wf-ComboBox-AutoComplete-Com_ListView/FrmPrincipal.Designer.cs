675b9896e61eee4d1ab4124420a85eba4e5e049b
José Junior <jeyjr.dg@gmail.com>
2023-12-03 15:58:11 -0300

    winForms_ComboBox

---
+++ WinForms/wf-ComboBox/wf-ComboBox-AutoComplete-Com_ListView/wf-ComboBox-AutoComplete-Com_ListView/FrmPrincipal.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace wf_ComboBox_AutoComplete_Com_ListView
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox1 = new ComboBox();
+            listView1 = new ListView();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            comboBox1.DropDownHeight = 1;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.IntegralHeight = false;
+            comboBox1.Location = new Point(12, 41);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(371, 23);
+            comboBox1.TabIndex = 0;
+            comboBox1.DropDown += comboBox1_DropDown;
+            comboBox1.DropDownClosed += comboBox1_DropDownClosed;
+            comboBox1.TextChanged += comboBox1_TextChanged;
+            comboBox1.KeyDown += comboBox1_KeyDown;
+            comboBox1.Leave += comboBox1_Leave;
+            // 
+            // listView1
+            // 
+            listView1.Activation = ItemActivation.OneClick;
+            listView1.HotTracking = true;
+            listView1.HoverSelection = true;
+            listView1.Location = new Point(12, 64);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(371, 167);
+            listView1.TabIndex = 2;
+            listView1.TileSize = new Size(370, 15);
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Tile;
+            listView1.Visible = false;
+            listView1.Click += listView1_Click;
+            listView1.KeyDown += listView1_KeyDown;
+            // 
+            // FrmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(0, 64, 64);
+            ClientSize = new Size(395, 452);
+            Controls.Add(listView1);
+            Controls.Add(comboBox1);
+            Name = "FrmPrincipal";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Principal";
+            Load += FrmPrincipal_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private ListView listView1;
+    }
+}
---
+++ WinForms/wf-ComboBox/wf-ComboBox-AutoComplete-Com_ListView/wf-ComboBox-AutoComplete-Com_ListView/FrmPrincipal.cs
@@ -0,0 +1,201 @@
+using System.Data;
+using System.Drawing.Text;
+
+namespace wf_ComboBox_AutoComplete_Com_ListView
+{
+    public partial class FrmPrincipal : Form
+    {
+        DataTable dadosOriginais = new DataTable();
+
+        string textDigitado = "";
+        bool dropdownAberto = false;
+        private int ID = 0;
+        private string Nome = "";
+
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        #region Metodos
+        private void BindAutoComplete()
+        {
+            listView1.Items.Clear();
+            string filtro = comboBox1.Text.Trim();
+
+            if (filtro.Trim() == "")
+            {
+                dadosOriginais.Rows
+                .OfType<DataRow>()
+                .ToList()
+                .ForEach(item => listView1.Items.Add(item["Nome"].ToString()));
+            }
+            else
+            {
+                dadosOriginais.Rows
+                .OfType<DataRow>()
+                .ToList()
+                .ForEach(item =>
+                {
+                    string n = item["Nome"].ToString().ToLower().Trim();
+                    string f = filtro.ToLower().Trim();
+
+                    if (n.Contains(f))
+                    {
+                        listView1.Items.Add(item["Nome"].ToString());
+                    }
+                });
+            }
+        }
+        private void SelecionarItemDoAutoComplete()
+        {
+            if (listView1.SelectedItems != null)
+            {
+                string itemSelecionado = listView1.SelectedItems[0].Text;
+                comboBox1.Text = itemSelecionado;
+                //int indice = comboBox1.FindString(itemSelecionado);
+
+                foreach (DataRow row in dadosOriginais.Rows)
+                {
+                    if (row["Nome"].ToString() == itemSelecionado)
+                    {
+                        ID = Convert.ToInt32(row["ID"].ToString());
+                        Nome = row["Nome"].ToString();
+
+                        break;
+                    }
+                }
+                FecharAutoComplete();
+            }
+        }
+        private void ExibirAutoComplete()
+        {
+            BindAutoComplete();
+            listView1.Visible = true;
+        }
+        private void FecharAutoComplete()
+        {
+            listView1.Visible = false;
+        }
+        #endregion Metodos
+
+        #region Eventos
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+            dadosOriginais = Tabela.ObterTabela();
+
+            comboBox1.DataSource = dadosOriginais;
+            comboBox1.ValueMember = "ID";
+            comboBox1.DisplayMember = "Nome";
+
+            comboBox1.Text = "";
+
+            //this.Click += FecharAutoComplete_Click;
+        }
+        private void FecharAutoComplete_Click(object sender, EventArgs e)
+        {
+            FecharAutoComplete();
+        }
+
+        /* comboBox */
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text.Length > 0)
+            {
+                if (dropdownAberto)
+                {
+                    textDigitado = comboBox1.Text;
+                    comboBox1.Text = "";
+                    comboBox1.DropDownHeight = 1;
+                    comboBox1.DroppedDown = false;
+
+                    if (comboBox1.SelectedItem == null)
+                    {
+                        comboBox1.Text = textDigitado;
+                        comboBox1.Select(comboBox1.Text.Length, 0);
+                        ExibirAutoComplete();
+                    }
+                }
+                else
+                {
+                    if (comboBox1.SelectedItem == null)
+                        ExibirAutoComplete();
+                }
+            }
+            else
+            {
+                FecharAutoComplete();
+            }
+        }
+        private void comboBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            /* Não permite navegar nos items do comboBox */
+            /* A "Navegação" só será permitida no ListViewr */
+
+            if (e.KeyCode == Keys.Up)
+            {
+                e.Handled = true;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                ExibirAutoComplete();
+
+                if (listView1.Items.Count > 0 && listView1.Visible)
+                {
+                    listView1.Focus();
+                    listView1.Items[0].Selected = true;
+                }
+
+                e.Handled = true;
+            }
+        }
+        private void comboBox1_Leave(object sender, EventArgs e)
+        {
+            //if (!listView1.Focused && !comboBox1.Focused)
+            //    FecharAutoComplete();
+        }
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+            FecharAutoComplete();
+            comboBox1.DropDownHeight = 140;
+
+            dropdownAberto = true;
+        }
+        private void comboBox1_DropDownClosed(object sender, EventArgs e)
+        {
+            comboBox1.DropDownHeight = 1;
+
+            dropdownAberto = false;
+        }
+
+        /* listView */
+        private void listView1_Click(object sender, EventArgs e)
+        {
+            SelecionarItemDoAutoComplete();
+        }
+        private void listView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            /* Quando chegar no indice 0 retorna para o comboBox */
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                SelecionarItemDoAutoComplete();
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                if (listView1.Items.Count > 0 && listView1.Items[0].Selected)
+                {
+                    comboBox1.Focus();
+                    comboBox1.Select(comboBox1.Text.Length, 0);
+
+                    if (comboBox1.Text.Length <= 0)
+                    {
+                        FecharAutoComplete();
+                    }
+                }
+            }
+        }
+
+        #endregion Eventos
+    }
+}
