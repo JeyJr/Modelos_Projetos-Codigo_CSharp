2a321f8dda5ccca9ae648c34c56e17c475cbe640
José Junior <jeyjr.dg@gmail.com>
2023-12-03 18:47:30 -0300

    ApiModeloBase

---
+++ AspNet/AspNet_Api_ModeloBase/Controllers/FilmesController.cs
@@ -0,0 +1,55 @@
+﻿using AspNet_Api_ModeloBase.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet_Api_ModeloBase.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmesController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 0;
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+
+            return CreatedAtAction(nameof(ObterFilme),
+                    new { id = filme.Id },
+                    filme);
+
+            //Postman teste, body (raw)
+            //https://localhost:7106/Filmes
+            //{
+            //    "Titulo" : "The Matrix",
+            //    "Genero" : "Ação"
+            //}
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> ObterFilmes(int skip = 0, int take = 50)
+        {
+            return filmes.Skip(skip).Take(take);
+
+            //Postman exemplo de url: https://localhost:7106/Filmes?skip=0&take=10
+            //Postman exemplo de url: https://localhost:7106/Filmes?take=10
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterFilme(int id)
+        {
+            var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if (filme == null) 
+                return NotFound();
+
+            return Ok(filme);
+
+            //Postman exemplo de url: https://localhost:7106/Filmes/2
+            //id seria o valor após Filmes/  
+            //no exemplo da url o id passado na requisicao é o 2
+        }
+    }
+}
---
+++ AspNet/AspNet_Api_ModeloBase/Program.cs
@@ -0,0 +1,42 @@
+using AspNet_Api_ModeloBase.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace AspNet_Api_ModeloBase
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var connectionString = builder.Configuration.GetConnectionString("JJConnection");
+
+            builder.Services.AddDbContext<FilmeContext>(opts =>
+            opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
