f5dc78ca4f0033ad1792e389c9339ca179b40f71
José Junior <jeyjr.dg@gmail.com>
2023-12-14 20:35:51 -0300

    FlyoutPage - Navegacao paginas

--- Maui/AppFlyoutPage/Menu.xaml.cs
+++ Maui/AppFlyoutPage/Menu.xaml.cs
@@ -6,4 +6,28 @@ public partial class Menu : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void btnPg1_Clicked(object sender, EventArgs e)
+    {
+        if (App.Current.MainPage is FlyoutPage flyout && flyout != null)
+        {
+            flyout.Detail = new Page1();
+        }
+    }
+
+    private void btnPg2_Clicked(object sender, EventArgs e)
+    {
+        if(App.Current.MainPage is FlyoutPage flyout && flyout != null)
+        {
+            flyout.Detail = new Page2();
+        }
+    }
+
+    private void btnPg3_Clicked(object sender, EventArgs e)
+    {
+        if (App.Current.MainPage is FlyoutPage flyout && flyout != null)
+        {
+            flyout.Detail = new Page3();
+        }
+    }
 }
