50d7e8f6147ec1d6b8b7233c7423cf1552848e98
José Junior <jeyjr.dg@gmail.com>
2023-12-14 18:42:42 -0300

    SetupInitial

---
+++ Maui/FlyoutPage/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FlyoutPage
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
