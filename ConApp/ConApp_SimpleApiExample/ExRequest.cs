e56f31053ff506ab70425a64e8fad5732603a4b1
José Junior <jeyjr.dg@gmail.com>
2024-01-29 19:54:17 -0300

    Exemplo de request

---
+++ ConApp/ConApp_SimpleApiExample/ExRequest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConApp_SimpleApiExample
+{
+    public class ExRequest
+    {
+        public async void RequestDocumento()
+        {
+            string requestURL = "https://enderecodaapi";
+            string accessToken = "token de login ou acesso";
+            string documentID = "id do documento que sera pesquisado na api";
+
+            string authorization = $"Bearer {accessToken}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                //Add varios parametros no header
+                client.DefaultRequestHeaders.Add("Authorization", authorization);
+                client.DefaultRequestHeaders.Add("api key", accessToken);
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync($"{requestURL}{documentID}");
+                    response.EnsureSuccessStatusCode();
+
+                    string responseString = await response.Content.ReadAsStringAsync();
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Request error: {e.Message}");
+                }
+            }
+        }
+    }
+}
