d612cdff7227cc2c880d979c797fe6f1fbc88c13
José Junior <jeyjr.dg@gmail.com>
2024-02-06 22:53:28 -0300

    Add ValidationResult modelo base

---
+++ WinForms/wf-ValidationResult/FrmPrincipal.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace wf_ValidationResult
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Location = new System.Drawing.Point(282, 212);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(143, 41);
+            this.btnImprimir.TabIndex = 0;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 265);
+            this.Controls.Add(this.btnImprimir);
+            this.Name = "FrmPrincipal";
+            this.Text = "Principal";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImprimir;
+    }
+}
+
---
+++ WinForms/wf-ValidationResult/FrmPrincipal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wf_ValidationResult
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Imprimir()
+        {
+            var imprimir = new Imprimir()
+            {
+                Assinatura = "JeyJr",
+                TipoPapel = "A4",
+                Documento = null,
+            };
+
+            var result = imprimir.Validate(imprimir, CultureInfo.CurrentCulture);
+
+            if (result.IsValid)
+            {
+                MessageBox.Show("Objeto validado!");
+            }
+            else
+            {
+                MessageBox.Show(result.ErrorContent.ToString());
+            }
+        }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+            Imprimir();
+        }
+    }
+}
