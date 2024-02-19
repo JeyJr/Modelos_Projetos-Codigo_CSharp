9483edbb7102f373d3c5c647743bebb343e6f909
José Junior <jeyjr.dg@gmail.com>
2024-02-19 06:44:05 -0300

    exemplo stacklayout

---
+++ Maui/TiposLayout/Controls/CustomComponentsControl.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace TiposLayout.Controls
+{
+    public static class CustomComponentsControl
+    {
+        public static View GetCustomTitle(string title, float size)
+        {
+            //var stackLayout = new StackLayout
+            //{
+            //    Orientation = StackOrientation.Horizontal,
+            //    HorizontalOptions = LayoutOptions.Fill,
+            //    VerticalOptions = LayoutOptions.Center
+            //};
+
+            //var btnExample = new Button()
+            //{
+            //    Text = "Exemplo de Botão",
+            //    FontSize = size,
+            //    FontFamily = "OpenSansRegular",
+            //    TextColor = Color.FromRgba(1, 1, 1, 1),
+            //    BackgroundColor = Color.FromRgba(0, 1, 0, 1),
+            //    HorizontalOptions = LayoutOptions.Start
+            //};
+
+            var lblTitle = new Label()
+            {
+                Text = title,
+                FontSize = size,
+                FontFamily = "OpenSansRegular",
+                TextColor = Color.FromArgb("000000"),
+                FontAttributes = FontAttributes.None,
+                VerticalTextAlignment = TextAlignment.Center,
+                HorizontalTextAlignment = TextAlignment.Center,
+                BackgroundColor = Color.FromArgb("8AE28A"),
+                HorizontalOptions = LayoutOptions.End,
+            };
+
+
+            //stackLayout.Children.Add(lblTitle);
+            //stackLayout.Children.Add(btnExample);
+
+            return lblTitle;
+        }
+    }
+}
--- Maui/TiposLayout/Views/StackLayoutPage.xaml.cs
+++ Maui/TiposLayout/Views/StackLayoutPage.xaml.cs
@@ -1,9 +1,14 @@
+using TiposLayout.Controls;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
 namespace TiposLayout.Views;
 
+[XamlCompilation(XamlCompilationOptions.Compile)]
 public partial class StackLayoutPage : ContentPage
 {
 	public StackLayoutPage()
 	{
 		InitializeComponent();
-	}
+        NavigationPage.SetTitleView(this, CustomComponentsControl.GetCustomTitle("Stack Laytout Page", 12));
+    }
 }
