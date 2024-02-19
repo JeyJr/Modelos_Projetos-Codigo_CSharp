437b2343b1b2af33f874aae898bf31a5b827dd99
José Junior <jeyjr.dg@gmail.com>
2024-02-19 18:46:30 -0300

    Add Absolute Layout

--- Maui/TiposLayout/Views/AbsoluteLayoutPage.xaml.cs
+++ Maui/TiposLayout/Views/AbsoluteLayoutPage.xaml.cs
@@ -1,3 +1,5 @@
+using TiposLayout.Controls;
+
 namespace TiposLayout.Views;
 
 public partial class AbsoluteLayoutPage : ContentPage
@@ -5,5 +7,6 @@ public partial class AbsoluteLayoutPage : ContentPage
 	public AbsoluteLayoutPage()
 	{
 		InitializeComponent();
-	}
+        NavigationPage.SetTitleView(this, CustomComponentsControl.GetCustomTitle("Absolute Layout Page", 12));
+    }
 }
